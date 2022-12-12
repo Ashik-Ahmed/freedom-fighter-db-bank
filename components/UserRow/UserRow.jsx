6c935c94bb64a7f7b6038bc99793d66e61ecd8b5
ASHIK AHMED <ashikahmed121@gmail.com>
2022-12-13 00:48:06 +0600

    showing users and delete user by id implemented

--- components/UserRow/UserRow.jsx
+++ components/UserRow/UserRow.jsx
@@ -3,25 +3,33 @@ import { FaEdit } from 'react-icons/fa';
 import { BsInfoSquareFill } from 'react-icons/bs';
 import { RiDeleteBin6Fill } from 'react-icons/ri';
 import { useRouter } from 'next/router';
+import Cookies from 'universal-cookie';
 
-const FreedomFighterRow = ({ freedomFighter, refreshData }) => {
+const UserRow = ({ user }) => {
 
-    const [deleteModal, setDeleteModal] = useState()
+    const router = useRouter();
+    const cookie = new Cookies();
 
-    const { _id, name, status, force, officialRank, freedomFighterRank, invited } = freedomFighter;
+    const [deleteModal, setDeleteModal] = useState(null);
 
-    const router = useRouter()
+    const { _id, name, email, role } = user;
 
 
-    //delete a freedom fighter
-    const deleteFreedomFighter = (id) => {
-        fetch(`http://localhost:5000/api/v1/freedomFighters/${id}`, {
+    const deleteUser = async (id) => {
+        await fetch(`http://localhost:5000/api/v1/users/${id}`, {
             method: 'DELETE',
+            headers: {
+                authorization: `Bearer ${cookie.get('TOKEN')}`
+            }
         })
             .then(res => res.json())
             .then(data => {
-                console.log(data);
-                refreshData();
+                if (data.data.deletedCount > 0) {
+                    console.log('Succefully Deleted User')
+                }
+                if (data.error) {
+                    console.log(data.error);
+                }
             })
         setDeleteModal(null)
     }
@@ -29,15 +37,12 @@ const FreedomFighterRow = ({ freedomFighter, refreshData }) => {
     return (
         <tr className='border-b text-gray-500'>
             <td className='p-2'>{name}</td>
-            <td>{force}</td>
-            <td>{officialRank.rank}</td>
-            <td>{freedomFighterRank.rank}</td>
-            <td className={status == 'Dead' ? 'text-red-600' : ''}>{status}</td>
-            <td>{invited.length} Times</td>
+            <td>{email}</td>
+            <td>{role}</td>
             <td className='flex gap-x-3 p-2'>
-                <BsInfoSquareFill size='24' color='#070225' className='cursor-pointer' onClick={() => router.push(`/freedom-fighters/${_id}/details`)} />
+                <BsInfoSquareFill size='24' color='#070225' className='cursor-pointer' onClick={() => router.push(`/manage-users/${user._id}/details`)} />
                 <FaEdit size='24' color='#00AA88' className='cursor-pointer' />
-                <label onClick={() => setDeleteModal(freedomFighter)} for="delete-modal">
+                <label onClick={() => setDeleteModal(user)} for="delete-modal">
                     <RiDeleteBin6Fill size='24' color='#DF5353' className='cursor-pointer' />
                 </label>
                 {
@@ -49,7 +54,7 @@ const FreedomFighterRow = ({ freedomFighter, refreshData }) => {
                             <h1 class="text-xl mb-4 font-bold text-slate-500">Do you Want to Delete?</h1>
                             <p className='text-xl font-semibold my-4 text-slate-500'>{name}</p>
                             <button onClick={() => setDeleteModal(null)} class="bg-red-500 px-4 py-2 rounded-md text-md text-white">Cancle</button>
-                            <button onClick={() => deleteFreedomFighter(_id)} class="bg-primary px-7 py-2 ml-2 rounded-md text-md text-white font-semibold">Ok</button>
+                            <button onClick={() => deleteUser(_id)} class="bg-primary px-7 py-2 ml-2 rounded-md text-md text-white font-semibold">Ok</button>
                         </div>
                     </div>
                 }
@@ -58,4 +63,4 @@ const FreedomFighterRow = ({ freedomFighter, refreshData }) => {
     );
 };
 
-export default FreedomFighterRow;
+export default UserRow;
--- pages/manage-users/index.jsx
+++ pages/manage-users/index.jsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import Cookies from 'universal-cookie';
+import UserRow from '../../components/UserRow/UserRow';
+import { SiAddthis } from 'react-icons/si';
 
-const index = () => {
+const ManageUsers = () => {
+
+    const [users, setUsers] = useState();
+    const [totalData, setTotalData] = useState(10);
+    const [currentPage, setCurrentPage] = useState(0)
+
+    const cookie = new Cookies();
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/v1/users', {
+            headers: {
+                authorization: `Bearer ${cookie.get('TOKEN')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => { setUsers(data.data) })
+    }, [])
+
+
+    const handlePageClick = (e) => {
+        setCurrentPage(parseInt(e?.selected) + 1);
+    }
+
+    const pageCount = Math.ceil(totalData / 10);
+
     return (
-        <div>
-            <p>Manage Users here</p>
+        <div className='max-w-6xl mx-auto my-4'>
+            <div className='bg-primary p-2 rounded-md inline-block mb-2'>
+                <Link href={`/manage-users/add-new`} className='flex items-center gap-x-2'><SiAddthis /> Add New User</Link>
+            </div>
+            <table className="table-auto container w-full mx-auto shadow-md">
+                <thead className='bg-primary '>
+                    <tr className='w-full text-left rounded-t-md'>
+                        <th className='p-2 rounded-tl-md'>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th className='rounded-tr-md'>Action</th>
+                    </tr>
+                </thead>
+                <tbody className='border bg-white'>
+                    {
+                        users && users?.map(user => <UserRow key={user._id} user={user}></UserRow>)
+                    }
+                </tbody>
+            </table>
+            <div className='flex w-full justify-between text-primary p-2 bg-gray-100 rounded-b-md'>
+                <div>
+                    <p>Showing {users?.length} of {totalData} data</p>
+                </div>
+                <ReactPaginate
+                    breakLabel="..."
+                    nextLabel="next >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={3}
+                    pageCount={pageCount}
+                    previousLabel="< previous"
+                    renderOnZeroPageCount={null}
+                    className='flex gap-x-4 justify-end p-1'
+                    activeClassName='bg-primary text-white px-2 rounded-md font-semibold'
+                />
+            </div>
         </div>
     );
 };
 
-export default index;
+export default ManageUsers;
