1b459d781cdd6f4b6a9c6fb8bc3964fe11ec7656
ASHIK AHMED <ashikahmed121@gmail.com>
2023-03-10 23:32:13 +0600

    edit event functinality implemented

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -17,6 +17,8 @@ const Events = () => {
     const [addEventModal, setAddEventModal] = useState(false)
     const [addEmailModal, setAddEmailModal] = useState(false)
     const [editEventModal, setEditEventModal] = useState(false)
+    const [mailBody, setMailBody] = useState(false)
+    const [eventDescription, seteventDescription] = useState(false)
 
     const getAllEvents = () => {
         setLoading(true)
@@ -26,6 +28,27 @@ const Events = () => {
                 console.log(data);
                 setEvents(data.data)
                 setLoading(false)
+            })
+    }
+
+    const updateEventFunction = (data) => {
+        const url = 'http://localhost:5000/api/v1/event'
+        fetch(url, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${cookie.get('TOKEN')}`
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.data.modifiedCount) {
+                    getAllEvents()
+                    setAddEmailModal(false)
+                    setEditEventModal(false)
+                }
+                console.log(data);
             })
     }
 
@@ -79,35 +102,41 @@ const Events = () => {
 
     const handleAddEmail = (e) => {
         e.preventDefault()
+        console.log(event);
         console.log(e.target.emailBody.value);
 
         const updateEventData = {
-            email: e.target.emailBody.value
+            eventId: event._id,
+            updatedData: {
+                emailBody: e.target.emailBody.value
+            }
+        }
+        updateEventFunction(updateEventData)
+    }
+
+
+    const handleEditEvent = (e) => {
+        e.preventDefault()
+
+        const updateEventData = {
+            eventId: event._id,
+            updatedData: {
+                description: eventDescription,
+                emailBody: mailBody
+            }
         }
 
-        const url = 'http://localhost:5000/api/v1/event'
-        fetch(url, {
-            method: 'PATCH',
-            headers: {
-                'content-type': 'application/json',
-                authorization: `Bearer ${cookie.get('TOKEN')}`
-            },
-            body: JSON.stringify(updateEventData)
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-            })
+        updateEventFunction(updateEventData)
     }
 
     const emailBodyTemplate = (rowData) => {
-        if (rowData.email) {
+        if (rowData.emailBody) {
             return <p>Email Available</p>
         }
         else {
             return (
                 <Button onClick={() => {
-                    setEvent(rowData.name)
+                    setEvent(rowData)
                     setAddEmailModal(true)
                 }} icon='pi pi-plus' className='p-button-sm'>Add Email</Button>
             )
@@ -117,7 +146,12 @@ const Events = () => {
     const actionBodyTemplate = (rowData) => {
         return (
             <div className='flex space-x-2'>
-                <Button icon='pi pi-pencil'></Button>
+                <Button icon='pi pi-pencil' onClick={() => {
+                    setEvent(rowData)
+                    seteventDescription(rowData.description)
+                    setMailBody(rowData?.emailBody)
+                    setEditEventModal(true);
+                }}></Button>
                 <Button icon='pi pi-trash' className='p-button-danger'></Button>
             </div>
         )
@@ -164,21 +198,43 @@ const Events = () => {
                 </div>
 
                 {/* add email dialogue  */}
-                <Dialog header={`Add Email for "${event}"`} visible={addEmailModal} onHide={() => {
+                <Dialog header={`Add Email for "${event?.name}"`} visible={addEmailModal} onHide={() => {
                     setAddEmailModal(false);
                     setEvent(null)
                 }} breakpoints={{ '960px': '75vw' }} style={{ width: '80vw' }} >
                     <form onSubmit={handleAddEmail} className='flex flex-col mt-4'>
                         <span className="p-float-label">
-                            <InputTextarea id="email" name='emailBody' className='w-full' rows={5} />
+                            <InputTextarea id="email" name='emailBody' className='w-full' rows={5} required />
                             <label htmlFor="email">Email Body</label>
                         </span>
-                        <div className='flex justify-end gap-2 mt-8'>
-                            <Button label="Cancel" icon="pi pi-times" onClick={() => {
-                                setAddEmailModal(false);
-                                setEvent(null)
-                            }} className="p-button-danger p-button-sm" />
-                            <Button type='submit' label="Submit" icon="pi pi-check" className='p-button-info p-button-sm' />
+                        <div className='flex justify-end gap-2 mt-4'>
+                            <Button type='submit' label="Submit" className='p-button-info p-button-sm' />
+                        </div>
+                    </form>
+                </Dialog>
+
+                {/* edit Event dialogue  */}
+                <Dialog header={`Update Event`} visible={editEventModal} onHide={() => {
+                    setEditEventModal(false);
+                    setEvent(null)
+                    seteventDescription(null)
+                    setMailBody(null)
+                }} breakpoints={{ '960px': '75vw' }} style={{ width: '80vw' }} >
+                    <form onSubmit={handleEditEvent} className='flex flex-col gap-2'>
+                        <div className='flex flex-col'>
+                            <label className='text-gray-500'>Name</label>
+                            <InputText disabled placeholder={event?.name} />
+                        </div>
+                        <div >
+                            <label className='text-gray-500'>Description</label>
+                            <InputTextarea id="description" name='description' value={eventDescription} onChange={(e) => seteventDescription(e.target.value)} className='w-full' rows={2} />
+                        </div>
+                        <div >
+                            <label className='text-gray-500'>Email Body</label>
+                            <InputTextarea id="email" name='emailBody' value={mailBody} onChange={(e) => setMailBody(e.target.value)} className='w-full' rows={5} />
+                        </div>
+                        <div className='flex justify-end gap-2 mt-4'>
+                            <Button type='submit' label="Submit" className='p-button-info p-button-sm' />
                         </div>
                     </form>
                 </Dialog>
