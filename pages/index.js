33f5e5a5eef92e7fe5b9acdc007d112a70ac6054
ASHIK AHMED <ashikahmed121@gmail.com>
2023-03-15 23:30:53 +0600

    third card color changed

--- pages/index.js
+++ pages/index.js
@@ -396,7 +396,7 @@ export default function Home() {
             </div>
           </div>
           <div className='w-full'>
-            <div className=" shadow-xl bg-white/70 text-gray-800 rounded-md border-primary border-l-4">
+            <div className=" shadow-xl bg-white/70 text-gray-800 rounded-md border-green-500 border-l-4">
               <div className="card-body">
                 <p className='text-xl font-bold'>Freedom Fighter</p>
                 <p className='text-xl'>180</p>
