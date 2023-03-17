6a084a1733196a782f34b05d0964530c6cbf4adb
ASHIK AHMED <ashikahmed121@gmail.com>
2023-03-17 20:49:40 +0600

    open multiple accordion by single click issue solved

--- components/Sidebar/Menu.jsx
+++ components/Sidebar/Menu.jsx
@@ -2,8 +2,7 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import SubMenu from './SubMenu';
 
-const Menu = ({ menu, open, setOpen, index }) => {
-    console.log(open);
+const Menu = ({ menu, open, index }) => {
     const [accordionOpen, setAccordionOpen] = useState(false);
     return (
         menu?.items ?
@@ -22,21 +21,25 @@ const Menu = ({ menu, open, setOpen, index }) => {
                 <div
                     // ref={contentElement}
                     // style={{ height: height }}
-                    className={` overflow-hidden ml-3.5 ${accordionOpen ? 'visible' : 'hidden'}  transition-all duration-700`}>
+                    className={` overflow-hidden ml-3.5 ${accordionOpen ? 'visible' : 'hidden'}  transition-all ease-in duration-700`}>
                     {
                         menu?.items?.map((menuItem, index) => {
                             // <SubMenu key={index} menuItem={menuItem} open={open} />
-                            <Link
-                                href={menuItem?.link}
-                                className={` ${menuItem?.margin && "mt-4"} ${open && 'py-2'} group flex items-center text-sm  gap-3 font-medium  px-2 hover:bg-primary rounded-md ml-3`}>
-                                <div className=''>{React.createElement(menuItem?.icon, { size: "20" })}</div>
-                                <h2>
-                                    {menuItem?.name}
-                                </h2>
-                                <h2 className={`${open && "hidden"} absolute z-2 left-48 bg-primary font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit `}>
-                                    <p className='px-2 py-2'>{menuItem?.name}</p>
-                                </h2>
-                            </Link>
+                            return (
+                                <Link
+                                    key={index}
+                                    href={menuItem?.link}
+                                    className={` ${menuItem?.margin && "mt-4"} ${open && 'py-2'} group flex items-center text-sm  gap-3 font-medium  px-2 hover:bg-primary rounded-md ml-3`}>
+                                    <div className=''>{React.createElement(menuItem?.icon, { size: "20" })}</div>
+                                    <h2>
+                                        {menuItem?.name}
+                                    </h2>
+                                    <h2 className={`${open && "hidden"} absolute z-2 left-48 bg-primary font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit `}>
+                                        <p className='px-2 py-2'>{menuItem?.name}</p>
+                                    </h2>
+                                </Link>
+                            )
+
                         })
                     }
                 </div>
