67acaaf21daa3333fcf5d60c935efe44adc8f6e8
ASHIK AHMED <ashikahmed121@gmail.com>
2023-03-25 23:36:29 +0600

    login page design improved

--- components/Login/Login.jsx
+++ components/Login/Login.jsx
@@ -1,31 +1,33 @@
 import { Button } from 'primereact/button';
 import { Checkbox } from 'primereact/checkbox';
 import { InputText } from 'primereact/inputtext';
-import React from 'react';
+import React, { useState } from 'react';
 
-const Login = () => {
+const Login = ({ handleLogin }) => {
+
+    const [checked, setChecked] = useState(false)
+
     return (
 
-        <div className="flex align-items-center justify-content-center">
+        <div className="bg-[#EFF3F8] min-h-[100vh] flex align-items-center justify-content-center">
             <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
                 <div className="text-center mb-5">
-                    <img src="/assets/images/blocks/logos/hyper.svg" alt="hyper" height={50} className="mb-3" />
+                    {/* <img src="/assets/images/blocks/logos/hyper.svg" alt="hyper" height={50} className="mb-3" /> */}
+                    <i className='pi pi-telegram mb-3 text-7xl bg-primary rounded-full shadow-lg'></i>
                     <div className="text-900 text-3xl font-medium mb-3">Welcome Back</div>
-                    <span className="text-600 font-medium line-height-3">Don't have an account?</span>
-                    <a className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</a>
                 </div>
 
                 <div>
                     <label htmlFor="email" className="block text-900 font-medium mb-2">Email</label>
-                    <InputText id="email" type="text" placeholder="Email address" className="w-full mb-3" />
+                    <InputText id="email" name='email' type="text" placeholder="Email address" className="w-full mb-3" />
 
                     <label htmlFor="password" className="block text-900 font-medium mb-2">Password</label>
-                    <InputText id="password" type="password" placeholder="Password" className="w-full mb-3" />
+                    <InputText id="password" name='password' type="password" placeholder="Password" className="w-full mb-3" />
 
                     <div className="flex align-items-center justify-content-between mb-6">
                         <div className="flex align-items-center">
                             <Checkbox id="rememberme" onChange={e => setChecked(e.checked)} checked={checked} className="mr-2" />
-                            <label htmlFor="rememberme">Remember me</label>
+                            <label htmlFor="rememberme" className='text-gray-700'>Remember me</label>
                         </div>
                         <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot your password?</a>
                     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ import "primeflex/primeflex.css";
 import { Password } from 'primereact/password';
 import { InputText } from 'primereact/inputtext';
 import Sidebar from '../components/Sidebar/Sidebar';
+import Login from '../components/Login/Login';
 
 
 function MyApp({ Component, pageProps }) {
@@ -84,48 +85,49 @@ function MyApp({ Component, pageProps }) {
 
   else {
     return (
-      <div className="hero bg-[#EFF3F8] min-h-[100vh]">
-        <div className="hero-content flex-col">
-          <p className='text-primary underline text-2xl font-bold'>Please Login</p>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form onSubmit={handleLogin} className="card-body">
-              <div className="p-float-label p-input-icon-right mt-4">
-                {/* <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label> */}
-                {/* <input name='email' type="email" placeholder="email" className="input input-bordered" /> */}
-                <i className="pi pi-envelope" />
-                <InputText type="email" name='email' id='email' className=' bg-gray-200 text-gray-700 w-full' required />
-                <label htmlFor="email">*Email</label>
-              </div>
-              <div className="p-float-label mt-4">
-                {/* <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label> */}
-                {/* <input name='password' type="password" placeholder="password" className="input input-bordered" /> */}
-                {/* <Password name='password' id='password' onChange={(e) => setPassword(e.target.value)} toggleMask feedback={false} required className={`${passError}&& 'p-invalid'`} /> */}
-                <div className='p-float-label '>
-                  <Password onChange={(e) => setPassword(e.target.value)} toggleMask feedback={false} required className={`${passError}&& 'p-invalid'`} />
-                  <label htmlFor="password">*Password</label>
-                </div>
-                {
-                  passError && <p className='text-red-500 text-xs italic'>{passError}</p>
-                }
-              </div>
-              <div>
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <input type='submit' value='Login' className="btn btn-primary" />
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+      <Login handleLogin={handleLogin} />
+      // <div className="hero bg-[#EFF3F8] min-h-[100vh]">
+      //   <div className="hero-content flex-col">
+      //     <p className='text-primary underline text-2xl font-bold'>Please Login</p>
+      //     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      //       <form onSubmit={handleLogin} className="card-body">
+      //         <div className="p-float-label p-input-icon-right mt-4">
+      //           {/* <div className="form-control">
+      //           <label className="label">
+      //             <span className="label-text">Email</span>
+      //           </label> */}
+      //           {/* <input name='email' type="email" placeholder="email" className="input input-bordered" /> */}
+      //           <i className="pi pi-envelope" />
+      //           <InputText type="email" name='email' id='email' className=' bg-gray-200 text-gray-700 w-full' required />
+      //           <label htmlFor="email">*Email</label>
+      //         </div>
+      //         <div className="p-float-label mt-4">
+      //           {/* <div className="form-control">
+      //           <label className="label">
+      //             <span className="label-text">Password</span>
+      //           </label> */}
+      //           {/* <input name='password' type="password" placeholder="password" className="input input-bordered" /> */}
+      //           {/* <Password name='password' id='password' onChange={(e) => setPassword(e.target.value)} toggleMask feedback={false} required className={`${passError}&& 'p-invalid'`} /> */}
+      //           <div className='p-float-label '>
+      //             <Password onChange={(e) => setPassword(e.target.value)} toggleMask feedback={false} required className={`${passError}&& 'p-invalid'`} />
+      //             <label htmlFor="password">*Password</label>
+      //           </div>
+      //           {
+      //             passError && <p className='text-red-500 text-xs italic'>{passError}</p>
+      //           }
+      //         </div>
+      //         <div>
+      //           <label className="label">
+      //             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+      //           </label>
+      //         </div>
+      //         <div className="form-control mt-6">
+      //           <input type='submit' value='Login' className="btn btn-primary" />
+      //         </div>
+      //       </form>
+      //     </div>
+      //   </div>
+      // </div>
     )
   }
 }
