b615a66e3cd172dfd50266972b215f54aa2c8883
ASHIK AHMED <ashikahmed121@gmail.com>
2023-04-14 23:43:36 +0600

    login component fully done

--- components/Login/Login.jsx
+++ components/Login/Login.jsx
@@ -48,7 +48,7 @@ const Login = ({ handleLogin, passError }) => {
                 <div className="w-full surface-card py-8 px-5 sm:px-8" style={{ borderRadius: "53px" }}>
                     <div className="text-center mb-5">
                         {/* <img src="/demo/images/login/avatar.png" alt="Image" height="50" className="mb-3" /> */}
-                        <div className="text-900 text-3xl font-medium mb-3">Welcome, Isabel!</div>
+                        <div className="text-900 text-3xl font-medium mb-3">Hi, Welcome!</div>
                         <span className="text-600 font-medium">Sign in to continue</span>
                     </div>
                     <form onSubmit={handleLogin} >
