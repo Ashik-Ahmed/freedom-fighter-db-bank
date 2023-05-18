6b0205b166e6a5cc01afff93c254cd5246152a16
ASHIK AHMED <ashikahmed121@gmail.com>
2023-05-18 18:32:56 +0600

    getting proper formData after submitting form for report generation

--- pages/admin/reports/index.jsx
+++ pages/admin/reports/index.jsx
@@ -1,5 +1,7 @@
 import { Button } from 'primereact/button';
 import { Calendar } from 'primereact/calendar';
+import { Column } from 'primereact/column';
+import { DataTable } from 'primereact/datatable';
 import { Dropdown } from 'primereact/dropdown';
 import React, { useEffect, useState } from 'react';
 
@@ -19,38 +21,60 @@ const Reports = () => {
             })
     }, [])
 
-    const handleChange = (e) => {
+    const handleChange = (event) => {
         const { name, value } = event.target;
         setFormData({
-            ...formData, [name]: JSON.stringify(value)
+            ...formData, [name]: value
         });
+    }
+
+    const handleGenerateReport = (e) => {
+        e.preventDefault()
+        console.log(formData.year.getFullYear());
+        console.log(year);
     }
 
     return (
         <div>
-            Generate Report
+            <p className='text-xl font-bold mb-2'>Generate Report</p>
+            <div className='bg-white p-4 w-fit mx-auto rounded-md shadow-lg'>
 
-            <div className='flex gap-2'>
-                <div>
-                    <Dropdown name='program' options={events} optionLabel='name' value={event}
-                        onChange={(e) => {
-                            setEvent(e.value)
-                            handleChange(e)
-                        }}
-                        placeholder="*Select Program" className=' w-full' required />
-                </div>
-                <div>
-                    <Calendar name='year' id='year' value={year}
-                        onChange={(e) => {
-                            setYear(e.value)
-                            handleChange(e)
-                        }}
-                        view="year" dateFormat="yy" placeholder='*Year' className='w-full' required />
-                </div>
+                <form onSubmit={handleGenerateReport} className='flex gap-x-4'>
+                    <div>
+                        <Dropdown name='program' options={events} optionLabel='name' value={event}
+                            onChange={(e) => {
+                                setEvent(e.value)
+                                handleChange(e)
+                            }}
+                            placeholder="*Select Program" className=' w-full' required />
+                    </div>
+                    <div>
+                        <Calendar name='year' id='year' value={year}
+                            onChange={(e) => {
+                                setYear(e.value)
+                                handleChange(e)
+                            }}
+                            view="year" dateFormat="yy" placeholder='*Year' className='w-full' required />
+                    </div>
 
-                <Button label='Submit' className='p-button-info p-button-sm  normal-case' type="submit" />
+                    <Button label='Submit' className='p-button-info p-button-sm  normal-case' type="submit" />
+                </form>
             </div>
-        </div>
+
+
+            {
+                generatedReport &&
+                <div className='bg-white p-2 max-w-7xl mx-auto rounded-md shadow-lg mt-2 min-h-[70vh]'>
+                    <DataTable value={finalSelected} header={header} dataKey="id" size='small' responsiveLayout="scroll" scrollHeight="70vh" loading={loading} stripedRows>
+                        {/* <Column selectionMode="multiple" headerStyle={{ width: '3rem' }}></Column> */}
+                        {
+                            cols.map((col, index) => <Column key={index} field={col.field} header={col.header} />)
+                        }
+                        <Column header='Invitaion Status' body={invitationStatusBodyTemplate} exportable={false}></Column>
+                    </DataTable>
+                </div>
+            }
+        </div >
     );
 };
 
